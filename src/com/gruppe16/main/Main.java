2bae2d46031acde20b0a78e6d8cb888a49da3996
sondrehav <sondre.havellen@gmail.com>
2015-02-23 11:25:25 +0100

    FXML loader

--- src/com/gruppe16/main/Main.java
+++ src/com/gruppe16/main/Main.java
@@ -1,7 +1,9 @@
 package com.gruppe16.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -13,7 +15,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		Scene scene = new Scene(new Pane());
+		Scene scene = new Scene((AnchorPane) FXMLLoader.load(Main.class.getResource("/com/gruppe16/main/mainPane.fxml")), 800, 600);
 		arg0.setScene(scene);
 		arg0.show();
 	}
