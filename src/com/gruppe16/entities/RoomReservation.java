dc6133a6fae1aeb2679ccefbf0d2cecfe4e80776
Ragnhild Neset <ragcecnes@gmail.com>
2015-02-26 15:51:37 +0100

    Gjort ting i room og roomReserver, ikke så viktig

--- src/com/gruppe16/entities/RoomReservation.java
+++ src/com/gruppe16/entities/RoomReservation.java
@@ -4,10 +4,13 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 import com.gruppe16.database.DBConnect;
+import com.mysql.jdbc.ResultSet;
 
 public class RoomReservation {
+	
 
 	public static void addNew(int appid, int roomid){
 		String q = "INSERT INTO RoomReservation(appid, roomid) VALUES (?, ?)";
@@ -21,8 +24,21 @@ public class RoomReservation {
 		}
 	}
 	
-	public Room[] findRoom(LocalDate appdate, LocalTime fromtime, LocalTime totime, int capacity){
-		
+	public void findRoom(LocalDate appdate, LocalTime fromtime, LocalTime totime, int capacity){
+		ArrayList<String> booked = new ArrayList<String>();
+		String q = "SELECT roomid FROM RoomReservation JOIN Appointment ON(RoomReservation.appid = Appointment.appid);";
+		try{
+			PreparedStatement s = DBConnect.getConnection().prepareStatement(q);
+			ResultSet rs = (ResultSet) s.executeQuery();
+			while(rs.next()){
+				booked.add(rs.getString("roomid"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for(String booking:booked){
+			System.out.print(booking);
+		}
 		
 	}
 }
