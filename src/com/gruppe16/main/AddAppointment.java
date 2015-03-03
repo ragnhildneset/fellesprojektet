0a7991c1ddf9b126d8fdb40a4e655bce58402d94
bitsauce <marcus.loo.vergara@gmail.com>
2015-03-03 11:18:30 +0100

    Appointment validering implementert

--- src/com/gruppe16/main/AddAppointment.java
+++ src/com/gruppe16/main/AddAppointment.java
@@ -1,9 +1,12 @@
 package com.gruppe16.main;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -16,6 +19,10 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.InnerShadow;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
@@ -71,31 +78,51 @@ public class AddAppointment extends Application implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		roomTextField.setEditable(true);
 		sendBtn.setOnAction(new EventHandler<ActionEvent>(){
 			@Override
 			public void handle(ActionEvent event) {
-				/*if(!isValidDate()) {
-				}
-				
-				if(!isValidFromTime()) {
-					
-				}
-				
-				if(!isValidTitle()) {
-					
-				}
-				
-				if(!isValidDesc()) {
-					
-				}
-				
-				if(!isValidAttendees()) {
-					
-				}*/
-				
 				boolean valid = true;
 				if(titleTextField.getText().isEmpty()) {
 					titleTextField.setEffect(new InnerShadow(4.0, Color.RED));
+					valid = false;
+				}
+				
+				LocalDate date = datePicker.getValue();
+				if(date == null || date.isBefore(LocalDate.now())) {
+					datePicker.setEffect(new InnerShadow(4.0, Color.RED));
+					valid = false;
+				}
+				
+				if(descriptionTextArea.getText().isEmpty()) {
+					descriptionTextArea.setEffect(new InnerShadow(4.0, Color.RED));
+					valid = false;
+				}
+				
+				fromTextField.setEffect(null);
+				toTextField.setEffect(null);
+				if(fromTextField.getText().isEmpty()) {
+					fromTextField.setEffect(new InnerShadow(4.0, Color.RED));
+					valid = false;
+				}
+
+				if(toTextField.getText().isEmpty()) {
+					toTextField.setEffect(new InnerShadow(4.0, Color.RED));
+					valid = false;
+				}
+				
+				if(!fromTextField.getText().isEmpty() && !toTextField.getText().isEmpty()) {
+					int fromHour = Integer.parseInt(fromTextField.getText(0, 2)), toHour = Integer.parseInt(toTextField.getText(0, 2));
+					int fromMin = Integer.parseInt(fromTextField.getText(3, 5)), toMin = Integer.parseInt(toTextField.getText(3, 5));
+					if(fromHour > toHour || (fromHour == toHour && fromMin >= toMin)) {
+						toTextField.setEffect(new InnerShadow(4.0, Color.RED));
+						fromTextField.setEffect(new InnerShadow(4.0, Color.RED));
+						valid = false;
+					}
+				}
+				
+				if(roomTextField.getText().isEmpty()) {
+					roomTextField.setEffect(new InnerShadow(4.0, Color.RED));
 					valid = false;
 				}
 				
@@ -104,10 +131,246 @@ public class AddAppointment extends Application implements Initializable {
 				}
 			}
 		});
+		
 		cancelBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
 				stage.close();
+			}
+		});
+		
+		searchForRoomBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO
+			}
+		});
+		
+		titleTextField.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				titleTextField.setEffect(null);
+			}
+		});
+
+		datePicker.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				datePicker.setEffect(null);
+			}
+		});
+
+		descriptionTextArea.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				descriptionTextArea.setEffect(null);
+			}
+		});
+
+		fromTextField.setEditable(false);
+		fromTextField.setUserData(new Integer(0));
+		fromTextField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldValue, Boolean newValue) {
+				if(newValue) {
+					if(fromTextField.getText().isEmpty()) fromTextField.setText("00:00");
+					fromTextField.setUserData(new Integer(0));
+					fromTextField.positionCaret(0);
+					fromTextField.selectForward();
+					fromTextField.selectForward();
+				}
+			}
+		});
+		
+		fromTextField.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event)
+			{
+				if(event.getX() < 20) {
+					fromTextField.setUserData(new Integer(0));
+					fromTextField.positionCaret(0);
+					fromTextField.selectForward();
+					fromTextField.selectForward();
+				}
+				else {
+					fromTextField.setUserData(new Integer(3));
+					fromTextField.positionCaret(3);
+					fromTextField.selectForward();
+					fromTextField.selectForward();
+				}
+			}
+		});
+		
+		fromTextField.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event)
+			{
+				if(event.getX() < 20) {
+					fromTextField.setUserData(new Integer(0));
+					fromTextField.positionCaret(0);
+					fromTextField.selectForward();
+					fromTextField.selectForward();
+				}
+				else {
+					fromTextField.setUserData(new Integer(3));
+					fromTextField.positionCaret(3);
+					fromTextField.selectForward();
+					fromTextField.selectForward();
+				}
+			}
+		});
+
+		fromTextField.setOnKeyTyped(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event)
+			{
+				char c = event.getCharacter().charAt(0);
+				String beforeText = fromTextField.getText();
+				
+				Integer textPos = (Integer)fromTextField.getUserData();
+				if(textPos < 5) {
+					if(c >= '0' && c <= '9') {
+						fromTextField.setText(fromTextField.getText(0, textPos) + c + fromTextField.getText(textPos+1, fromTextField.getText().length()));
+					}
+				}
+				
+				if(Integer.parseInt(fromTextField.getText(0, 2)) >= 24 || Integer.parseInt(fromTextField.getText(3, 5)) >= 60) {
+					fromTextField.setText(beforeText);
+				}
+				else {
+					textPos += 1;
+					if(textPos == 2) {
+						textPos += 1;
+					}
+					else if(textPos >= 5) {
+						textPos = 0;
+						toTextField.requestFocus();
+					}
+					fromTextField.setUserData(textPos);
+				}
+				
+				if(textPos < 2) {
+					fromTextField.positionCaret(0);
+					fromTextField.selectForward();
+					fromTextField.selectForward();
+				}
+				else {
+					fromTextField.positionCaret(3);
+					fromTextField.selectForward();
+					fromTextField.selectForward();
+				}
+			}
+		});
+		
+		fromTextField.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				fromTextField.setEffect(null);
+			}
+		});
+		
+		toTextField.setEditable(false);
+		toTextField.setUserData(new Integer(0));
+		toTextField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldValue, Boolean newValue) {
+				if(newValue) {
+					if(toTextField.getText().isEmpty()) toTextField.setText("00:00");
+					toTextField.setUserData(new Integer(0));
+					toTextField.positionCaret(0);
+					toTextField.selectForward();
+					toTextField.selectForward();
+				}
+			}
+		});
+		
+		toTextField.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event)
+			{
+				if(event.getX() < 20) {
+					toTextField.setUserData(new Integer(0));
+					toTextField.positionCaret(0);
+					toTextField.selectForward();
+					toTextField.selectForward();
+				}
+				else {
+					toTextField.setUserData(new Integer(3));
+					toTextField.positionCaret(3);
+					toTextField.selectForward();
+					toTextField.selectForward();
+				}
+			}
+		});
+		
+		toTextField.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event)
+			{
+				if(event.getX() < 20) {
+					toTextField.setUserData(new Integer(0));
+					toTextField.positionCaret(0);
+					toTextField.selectForward();
+					toTextField.selectForward();
+				}
+				else {
+					toTextField.setUserData(new Integer(3));
+					toTextField.positionCaret(3);
+					toTextField.selectForward();
+					toTextField.selectForward();
+				}
+			}
+		});
+
+		toTextField.setOnKeyTyped(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event)
+			{
+				char c = event.getCharacter().charAt(0);
+				String beforeText = toTextField.getText();
+				
+				Integer textPos = (Integer)toTextField.getUserData();
+				if(textPos < 5) {
+					if(c >= '0' && c <= '9') {
+						toTextField.setText(toTextField.getText(0, textPos) + c + toTextField.getText(textPos+1, toTextField.getText().length()));
+					}
+				}
+				
+				if(Integer.parseInt(toTextField.getText(0, 2)) >= 24 || Integer.parseInt(toTextField.getText(3, 5)) >= 60) {
+					toTextField.setText(beforeText);
+				}
+				else {
+					textPos += 1;
+					if(textPos == 2) {
+						textPos += 1;
+					}
+					toTextField.setUserData(textPos);
+				}
+				
+				if(textPos < 2) {
+					toTextField.positionCaret(0);
+					toTextField.selectForward();
+					toTextField.selectForward();
+				}
+				else {
+					toTextField.positionCaret(3);
+					toTextField.selectForward();
+					toTextField.selectForward();
+				}
+			}
+		});
+		
+		toTextField.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				toTextField.setEffect(null);
+			}
+		});
+
+		roomTextField.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				roomTextField.setEffect(null);
 			}
 		});
 	}
