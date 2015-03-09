e179aba66e39a7d7bb9be6e9f48e0e84b6cb99fb
sondrehav <sondre.havellen@gmail.com>
2015-03-09 13:02:20 +0100

    Fixed the roomRes query

---
+++ src/com/gruppe16/main/Notification.java
@@ -0,0 +1,18 @@
+package com.gruppe16.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Notification implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
