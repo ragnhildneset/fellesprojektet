ab82208d573a6371826fbe19016a4e6e854677d0
bitsauce <marcus.loo.vergara@gmail.com>
2015-03-09 11:16:27 +0100

    Kaos fikset

--- src/com/gruppe16/main/CalendarMain.java
+++ src/com/gruppe16/main/CalendarMain.java
@@ -2,55 +2,30 @@ package com.gruppe16.main;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.PreparedStatement;
-import java.time.LocalDate;
 import java.util.Date;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ResourceBundle;
-
-import com.gruppe16.database.DBConnect;
-import com.gruppe16.entities.Employee;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Cursor;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextField;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.stage.WindowEvent;
+
+import com.gruppe16.entities.Employee;
 
 public class CalendarMain extends Application {
 	static String[] MONTH_NAMES = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
@@ -71,7 +46,10 @@ public class CalendarMain extends Application {
 	private Button findCalendarBtn;
 	
 	@FXML
-	private Button backToCalendarBtn;
+	private ImageView backToCalendarBtn;
+	
+	@FXML
+	private ImageView alertBtn;
 	
 	@FXML
 	private Button selectAllGroupsBtn;
@@ -99,8 +77,6 @@ public class CalendarMain extends Application {
 
 	private DayPlanView dayPlanView;
 	
-	private LocalDate someDate = LocalDate.now();
-	
 	private Employee employee;
 	
 	public CalendarMain() {
@@ -115,7 +91,7 @@ public class CalendarMain extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		URL url = getClass().getResource("/com/gruppe16/main/MainPane.fxml");
+		URL url = getClass().getResource("/com/gruppe16/main/CalendarMain.fxml");
 		
 		FXMLLoader fxmlLoader = new FXMLLoader();
 		fxmlLoader.setLocation(url);
@@ -196,6 +172,13 @@ public class CalendarMain extends Application {
 			}
 		});
 
+		nextDateBtn.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				nextDateBtn.setEffect(new ColorAdjust(0.0, 0.0, 0.2, 0.0));
+			}
+		});
+
 		prevDateBtn.setOnMouseEntered(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
@@ -214,6 +197,69 @@ public class CalendarMain extends Application {
 			@Override
 			public void handle(MouseEvent event) {
 				prevDateBtn.setEffect(new ColorAdjust(0.0, 0.0, -0.2, 0.0));
+			}
+		});
+
+		prevDateBtn.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				prevDateBtn.setEffect(new ColorAdjust(0.0, 0.0, 0.2, 0.0));
+			}
+		});
+
+		alertBtn.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				alertBtn.setEffect(new ColorAdjust(0.0, 0.0, 0.2, 0.0));
+			}
+		});
+
+		alertBtn.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				alertBtn.setEffect(null);
+			}
+		});
+
+		alertBtn.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				alertBtn.setEffect(new ColorAdjust(0.0, 0.0, -0.2, 0.0));
+			}
+		});
+
+		alertBtn.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				alertBtn.setEffect(new ColorAdjust(0.0, 0.0, 0.2, 0.0));
+			}
+		});
+
+		backToCalendarBtn.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				backToCalendarBtn.setEffect(new ColorAdjust(0.0, 0.0, 0.2, 0.0));
+			}
+		});
+
+		backToCalendarBtn.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				backToCalendarBtn.setEffect(null);
+			}
+		});
+
+		backToCalendarBtn.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				backToCalendarBtn.setEffect(new ColorAdjust(0.0, 0.0, -0.2, 0.0));
+			}
+		});
+
+		backToCalendarBtn.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				backToCalendarBtn.setEffect(new ColorAdjust(0.0, 0.0, 0.2, 0.0));
 			}
 		});
 	}
@@ -260,12 +306,12 @@ public class CalendarMain extends Application {
 		
 		backToCalendarBtn.setVisible(true);
 		
-		monthLabel.setText(date.toString());
+		monthLabel.setText(MONTH_NAMES[date.getMonth()] + " " + date.getDate());
 		yearLabel.setText(Integer.toString(calendarView.getYear()));
 
-		backToCalendarBtn.setOnAction(new EventHandler<ActionEvent>() {
+		backToCalendarBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
-			public void handle(ActionEvent evnet) {
+			public void handle(MouseEvent evnet) {
 				showCalendar(dayPlanView.getDate());
 			}
 		});
@@ -274,7 +320,7 @@ public class CalendarMain extends Application {
 			@Override
 			public void handle(MouseEvent evnet) {
 				dayPlanView.nextDay();
-				monthLabel.setText(date.toString());
+				monthLabel.setText(MONTH_NAMES[date.getMonth()] + " " + date.getDate());
 				yearLabel.setText(Integer.toString(calendarView.getYear()));
 				redraw();
 			}
@@ -284,7 +330,7 @@ public class CalendarMain extends Application {
 			@Override
 			public void handle(MouseEvent evnet) {
 				dayPlanView.prevDay();
-				monthLabel.setText(date.toString());
+				monthLabel.setText(MONTH_NAMES[date.getMonth()] + " " + date.getDate());
 				yearLabel.setText(Integer.toString(calendarView.getYear()));
 				redraw();
 			}
